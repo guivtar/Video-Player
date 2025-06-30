69a78c573d20c4131c998cc4e94eb8003bab1adb
Vivek <guivtar@gmail.com>
2025-06-30 19:29:25 +0530

    Initial Commit

---
+++ VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by Vivek Lakshmanan on 30/06/25.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VideoPlayerTests/VideoPlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerTests.swift
+//  VideoPlayerTests
+//
+//  Created by Vivek Lakshmanan on 30/06/25.
+//
+
+import Testing
+@testable import VideoPlayer
+
+struct VideoPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
